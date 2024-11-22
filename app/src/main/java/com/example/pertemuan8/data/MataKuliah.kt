e13a8330ee44a416517b8eb117e0e10f88d1f8d3
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-11-22 13:43:06 +0700

    Menambahkan pilihan mata kuliah pada tampilan di android

--- app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
+++ app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
@@ -4,6 +4,9 @@ object MataKuliah {
     val options = listOf(
         "Pemrograman Aplikasi Mobile Lanjut",
         "Pengembangan Aplikasi Web Framework",
-        
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
     )
 }
